a3c864cbc9f9b96caa07019b8ac96ea0abe1f898
Ariel Lorenzo <ariellorenzo08@gmail.com>
2020-03-31 13:50:44 -0400

    Updated 'app.py'; Text file upload and download now available

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ class Ui(QtWidgets.QMainWindow):
       # Ciphertext Output Label
       self.ciphertextLabel = self.findChild(QtWidgets.QLabel, 'CiphertextLabel')
 
+      # Upload Button
+      self.uploadTxtFileButton = self.findChild(QtWidgets.QPushButton, 'UploadTxtFileButton')
+      self.uploadTxtFileButton.clicked.connect(self.uploadHandler)
+
+      # Download Button
+      self.downloadTxtFileButton = self.findChild(QtWidgets.QPushButton, 'DownloadTxtFileButton')
+      self.downloadTxtFileButton.clicked.connect(self.downloadHandler)
+
       # Show the app dialog
       self.show()
    
@@ -45,6 +53,39 @@ class Ui(QtWidgets.QMainWindow):
          self.ciphertextLabel.setText("Ciphertext should appear here....")
       else:
          self.ciphertextLabel.setText(vigenereEncrypt(user_text, generateKey(user_text, phrase)))
+   
+   def uploadHandler(self):
+      """
+      Upload Button Handler
+      """
+      # Open file explorer
+      currentDir = os.getcwd()
+      # Open txt file and read contents
+      fpath, filters = QFileDialog.getOpenFileName(self, 'Open file', currentDir, "Text files (*.txt)")
+      # Put contents in the 'UserText' textbox
+      file = open(fpath, 'r')
+      self.plaintextBox.setText(str())
+      for line in file:
+         self.plaintextBox.setText(self.plaintextBox.toPlainText()+line)
+      
+      file.close()
+   
+   def downloadHandler(self):
+      """
+      Download Button Handler
+      """
+      # Write to a new file named 'ciphertext.txt'
+      # in the current directory
+      file = open(os.getcwd() + "\\ciphertext.txt", "w")
+      file.write(self.ciphertextLabel.text())
+      file.close()
+      # Tell the user the download is complete
+      msg = QMessageBox()
+      msg.setIcon(QMessageBox.Information)
+      msg.setText("Your ciphertext is ready!")
+      msg.setWindowTitle("Success!")
+      msg.setStandardButtons(QMessageBox.Ok)
+      msg.exec_()
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
